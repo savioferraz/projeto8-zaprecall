b011b0a771e885fdac65d990656f0b9e2dd1633f
savioferraz <savio.ferraz88@gmail.com>
2022-07-23 13:52:46 -0300

    projeto iniciado

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../assets/css/reset.css";
+import "./App.css";
+
+
+function App() {
+    return (
+        <>
+        <div>Olá mundo</div>
+        </>
+    )
+}
+
+export default App;
